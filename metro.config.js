89276e7317c6bb3d9f2ef7d70e17dbbb7e76b90f
Syed Ansar <syedansar6666@gmail.com>
2024-04-30 23:02:50 +0530

    Esa build

--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,20 @@
-const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+const { getDefaultConfig } = require('expo/metro-config')
 
-const defaultConfig = getDefaultConfig(__dirname);
-const { assetExts, sourceExts } = defaultConfig.resolver;
+module.exports = (async () => {
+	// eslint-disable-next-line no-undef
+	const config = await getDefaultConfig(__dirname)
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {
-  transformer: {
-    babelTransformerPath: require.resolve("react-native-svg-transformer")
-  },
-  resolver: {
-    assetExts: assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...sourceExts, "svg"]
-  }
-};
+	const { transformer, resolver } = config
 
-module.exports = mergeConfig(defaultConfig, config);
+	config.transformer = {
+		...transformer,
+		babelTransformerPath: require.resolve('react-native-svg-transformer'),
+	}
+	config.resolver = {
+		...resolver,
+		assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+		sourceExts: [...resolver.sourceExts, 'svg'],
+	}
+
+	return config
+})()
