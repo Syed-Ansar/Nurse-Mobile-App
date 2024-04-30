b0df12b2d6d4969f2888fa0bb1bb9b83c5ea2e43
Syed-Ansar <syedansar6666@gmail.com>
2024-04-30 08:21:59 +0530

    Stack screen added for Auth screens

--- src/app/(app)/drawer/help.tsx
+++ src/app/(app)/drawer/help.tsx
@@ -1,11 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { router } from 'expo-router'
 
 type Props = {}
 
 const Help = (props: Props) => {
 	return (
 		<View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+			<Button
+				title="Go back"
+				onPress={() => {
+					router.back()
+				}}
+			/>
 			<Text>Help</Text>
 		</View>
 	)
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,16 @@
-import { Slot } from 'expo-router'
+import { Stack } from 'expo-router'
+
 import { SessionProvider } from '../context/auth-context'
 
 export default function Root() {
 	// Set up the auth context and render our layout inside of it.
 	return (
 		<SessionProvider>
-			<Slot />
+			<Stack
+				screenOptions={{
+					headerShown: false,
+				}}
+			/>
 		</SessionProvider>
 	)
 }
