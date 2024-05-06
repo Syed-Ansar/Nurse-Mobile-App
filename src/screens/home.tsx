119fca3a7d06ac779c0826fdc3720f125197c182
Syed Ansar <syedansar6666@gmail.com>
2024-05-06 18:16:28 +0530

    View added

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -64,197 +64,199 @@ const HomeScreen = ({ navigation }: Props) => {
 	}, [])
 
 	return (
-		<SafeAreaView
-			style={{
-				paddingHorizontal: 15,
-				paddingVertical: 20,
-				backgroundColor: 'white',
-			}}
-		>
-			<View
+		<View>
+			<SafeAreaView
 				style={{
-					gap: 2,
+					paddingHorizontal: 15,
+					paddingVertical: 20,
+					backgroundColor: 'white',
 				}}
 			>
+				<View
+					style={{
+						gap: 2,
+					}}
+				>
+					<View
+						style={{
+							display: 'flex',
+							flexDirection: 'row',
+							alignItems: 'center',
+							gap: 4,
+						}}
+					>
+						<Text
+							style={{
+								fontSize: 24,
+								fontWeight: '700',
+							}}
+						>
+							Welcome !
+						</Text>
+						<WelcomeHand />
+					</View>
+					<Text
+						style={{
+							fontSize: 12,
+							fontWeight: '500',
+						}}
+					>
+						Emily Ava
+					</Text>
+				</View>
 				<View
 					style={{
 						display: 'flex',
 						flexDirection: 'row',
 						alignItems: 'center',
-						gap: 4,
+						justifyContent: 'space-between',
+						marginVertical: 20,
 					}}
 				>
-					<Text
-						style={{
-							fontSize: 24,
-							fontWeight: '700',
-						}}
-					>
-						Welcome !
-					</Text>
-					<WelcomeHand />
-				</View>
-				<Text
-					style={{
-						fontSize: 12,
-						fontWeight: '500',
-					}}
-				>
-					Emily Ava
-				</Text>
-			</View>
-			<View
-				style={{
-					display: 'flex',
-					flexDirection: 'row',
-					alignItems: 'center',
-					justifyContent: 'space-between',
-					marginVertical: 20,
-				}}
-			>
-				{HomeTab.map((item) => {
-					return (
-						<Pressable
-							onPress={() => {
-								navigation.navigate(item.route)
-							}}
-							key={item.name}
-							style={{
-								borderWidth: 0,
-								padding: 10,
-								alignSelf: 'flex-start',
-								borderRadius: 8,
-								borderLeftWidth: 2,
-								borderColor: item.color || '#000',
-							}}
-						>
-							<View
+					{HomeTab.map((item) => {
+						return (
+							<Pressable
+								onPress={() => {
+									navigation.navigate(item.route)
+								}}
+								key={item.name}
 								style={{
-									gap: 15,
-									display: 'flex',
-									flexDirection: 'row',
-									alignItems: 'center',
+									borderWidth: 0,
+									padding: 10,
+									alignSelf: 'flex-start',
+									borderRadius: 8,
+									borderLeftWidth: 2,
+									borderColor: item.color || '#000',
 								}}
 							>
-								<View>{item.icon}</View>
 								<View
 									style={{
-										gap: 5,
+										gap: 15,
+										display: 'flex',
+										flexDirection: 'row',
+										alignItems: 'center',
+									}}
+								>
+									<View>{item.icon}</View>
+									<View
+										style={{
+											gap: 5,
+										}}
+									>
+										<Text
+											style={{
+												fontSize: 16,
+												fontWeight: '500',
+											}}
+										>
+											{item.name}
+										</Text>
+										<View
+											style={{
+												display: 'flex',
+												flexDirection: 'row',
+												alignItems: 'center',
+												gap: 2,
+											}}
+										>
+											<Text
+												style={{
+													fontSize: 12,
+													fontWeight: '700',
+												}}
+											>
+												{item.amount}
+											</Text>
+											<Text
+												style={{
+													fontSize: 12,
+													fontWeight: '400',
+												}}
+											>
+												{item.time}
+											</Text>
+										</View>
+									</View>
+								</View>
+							</Pressable>
+						)
+					})}
+				</View>
+				<View
+					style={{
+						maxWidth: SCREEN_WIDTH,
+						backgroundColor: '#F2F4F7',
+						display: 'flex',
+						flexDirection: 'row',
+						justifyContent: 'space-between',
+						alignItems: 'center',
+						borderRadius: 8,
+					}}
+				>
+					{jobTabs.map((item, index) => {
+						return (
+							<React.Fragment key={item}>
+								<Pressable
+									key={item}
+									onPress={() => {
+										setActiveTab(item)
+										setJobs(JobData.filter((job) => job.status === item))
+									}}
+									style={{
+										borderWidth: 1,
+										borderColor: item === activeTab ? '#3513DD' : '#F2F4F7',
+										borderRadius: 6,
+										margin: 5,
 									}}
 								>
 									<Text
 										style={{
-											fontSize: 16,
-											fontWeight: '500',
+											paddingVertical: 8,
+											paddingHorizontal: 10,
+											fontSize: fontSize.xxs,
+											color: item === activeTab ? '#3513DD' : '#828998',
 										}}
 									>
-										{item.name}
+										{item} Jobs
 									</Text>
+								</Pressable>
+								{index !== 2 ? (
 									<View
 										style={{
-											display: 'flex',
-											flexDirection: 'row',
-											alignItems: 'center',
-											gap: 2,
+											...utilsStyles.itemSeparator,
+											borderWidth: 0.5,
+											height: '60%',
 										}}
-									>
-										<Text
-											style={{
-												fontSize: 12,
-												fontWeight: '700',
-											}}
-										>
-											{item.amount}
-										</Text>
-										<Text
-											style={{
-												fontSize: 12,
-												fontWeight: '400',
-											}}
-										>
-											{item.time}
-										</Text>
-									</View>
-								</View>
-							</View>
-						</Pressable>
-					)
-				})}
-			</View>
-			<View
-				style={{
-					maxWidth: SCREEN_WIDTH,
-					backgroundColor: '#F2F4F7',
-					display: 'flex',
-					flexDirection: 'row',
-					justifyContent: 'space-between',
-					alignItems: 'center',
-					borderRadius: 8,
-				}}
-			>
-				{jobTabs.map((item, index) => {
-					return (
-						<React.Fragment key={item}>
-							<Pressable
-								key={item}
-								onPress={() => {
-									setActiveTab(item)
-									setJobs(JobData.filter((job) => job.status === item))
-								}}
-								style={{
-									borderWidth: 1,
-									borderColor: item === activeTab ? '#3513DD' : '#F2F4F7',
-									borderRadius: 6,
-									margin: 5,
-								}}
-							>
-								<Text
-									style={{
-										paddingVertical: 8,
-										paddingHorizontal: 10,
-										fontSize: fontSize.xxs,
-										color: item === activeTab ? '#3513DD' : '#828998',
-									}}
-								>
-									{item} Jobs
-								</Text>
-							</Pressable>
-							{index !== 2 ? (
-								<View
-									style={{
-										...utilsStyles.itemSeparator,
-										borderWidth: 0.5,
-										height: '60%',
-									}}
-								/>
-							) : null}
-						</React.Fragment>
-					)
-				})}
-			</View>
-			<View
-				style={{
-					borderRadius: 10,
-					marginTop: 10,
-				}}
-			>
-				<ScrollView
-					refreshControl={
-						<RefreshControl refreshing={refreshing} onRefresh={onRefresh} colors={['#3513DD']} />
-					}
-					showsVerticalScrollIndicator={false}
-					contentInsetAdjustmentBehavior="never"
-					centerContent
-					contentContainerStyle={{
-						paddingBottom: 400,
+									/>
+								) : null}
+							</React.Fragment>
+						)
+					})}
+				</View>
+				<View
+					style={{
+						borderRadius: 10,
+						marginTop: 10,
 					}}
 				>
-					{jobs.map((item) => {
-						return <Job key={item.id} job={item} />
-					})}
-				</ScrollView>
-			</View>
-		</SafeAreaView>
+					<ScrollView
+						refreshControl={
+							<RefreshControl refreshing={refreshing} onRefresh={onRefresh} colors={['#3513DD']} />
+						}
+						showsVerticalScrollIndicator={false}
+						contentInsetAdjustmentBehavior="never"
+						centerContent
+						contentContainerStyle={{
+							paddingBottom: 400,
+						}}
+					>
+						{jobs.map((item) => {
+							return <Job key={item.id} job={item} />
+						})}
+					</ScrollView>
+				</View>
+			</SafeAreaView>
+		</View>
 	)
 }
 
